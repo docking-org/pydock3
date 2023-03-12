2aa4e12bdd4e06dfaf6d7b1e3e7733dd3efd1de2
Ian Scott Knight <isk@alumni.stanford.edu>
2023-03-12 16:56:31 -0700

    adds __repr__ to Parameter class to fix hashing of config dicts that have parameter objects as values

--- pydock3/config.py
+++ pydock3/config.py
@@ -31,6 +31,9 @@ class Parameter(object):
     def __str__(self):
         return str(self.value)
 
+    def __repr__(self):
+        return str(self.value)
+
     def __eq__(self, other):
         if type(other) == type(self):
             return self.value == other.value
