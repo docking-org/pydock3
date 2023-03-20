9b974a1f4187571805630d78f6c047f41b3e2ce0
Ian Scott Knight <isk@alumni.stanford.edu>
2023-03-20 12:04:32 -0700

    adds conditional addition of sourcing of scheduler settings script

--- pydock3/job_schedulers.py
+++ pydock3/job_schedulers.py
@@ -10,6 +10,7 @@ from subprocess import CompletedProcess
 import xmltodict
 
 from pydock3.util import system_call, get_nested_dict_item
+from pydock3.files import File
 
 #
 logger = logging.getLogger(__name__)
@@ -90,6 +91,8 @@ class SlurmJobScheduler(JobScheduler):
             else:
                 array_str = f"{contiguous_task_nums_set[0]}-{contiguous_task_nums_set[-1]}"
             command_str = f"{self.SBATCH_EXEC} --export=ALL -J {job_name} -o {out_log_dir_path}/{job_name}_%A_%a.out -e {err_log_dir_path}/{job_name}_%A_%a.err --signal=B:USR1@120 --array={array_str} {script_path}"
+            if File.file_exists(self.SLURM_SETTINGS):
+                command_str = f"source {self.SLURM_SETTINGS}; {command_str}"
             if job_timeout_minutes is None:
                 command_str += " --time=0"
             else:
@@ -175,7 +178,9 @@ class SGEJobScheduler(JobScheduler):
                 array_str = f"{contiguous_task_nums_set[0]}"
             else:
                 array_str = f"{contiguous_task_nums_set[0]}-{contiguous_task_nums_set[-1]}"
-            command_str = f"source {self.SGE_SETTINGS}; {self.QSUB_EXEC} -V -N {job_name} -o {out_log_dir_path} -e {err_log_dir_path} -cwd -S /bin/bash -q !gpu.q -t {array_str} {script_path}"
+            command_str = f"{self.QSUB_EXEC} -V -N {job_name} -o {out_log_dir_path} -e {err_log_dir_path} -cwd -S /bin/bash -q !gpu.q -t {array_str} {script_path}"
+            if File.file_exists(self.SGE_SETTINGS):
+                command_str = f"source {self.SGE_SETTINGS}; {command_str}"
             if job_timeout_minutes is not None:
                 job_timeout_seconds = 60 * job_timeout_minutes
                 command_str += (
