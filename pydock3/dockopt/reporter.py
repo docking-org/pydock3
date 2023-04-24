2a029e80263bbb56f6856775c477b8af59da037a
Ian Scott Knight <isk@alumni.stanford.edu>
2023-04-23 17:52:56 -0700

    fixes up HTML report plots (mostly titles and fonts and stuff)

--- pydock3/dockopt/reporter.py
+++ pydock3/dockopt/reporter.py
@@ -1,17 +1,14 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, NoReturn
+from typing import TYPE_CHECKING, NoReturn, Tuple, Union
 import os
 import itertools
 import re
 import base64
-from io import BytesIO
 
 import numpy as np
 import pandas as pd
 from scipy.interpolate import griddata
-from PIL import Image
 import plotly.graph_objs as go
-import plotly.express as px
 
 from pydock3.util import get_ordinal, sort_list_by_another_list
 from pydock3.retrodock.retrodock import ROC_PLOT_FILE_NAME, ENERGY_PLOT_FILE_NAME, CHARGE_PLOT_FILE_NAME
@@ -42,6 +39,30 @@ def get_sorted_component_ids(component_ids: list) -> list:
     return sort_list_by_another_list(component_ids, numeric_ids)  # sort by numeric ids, return component ids
 
 
+def get_axis_label(param):
+    label = ""
+    barrier = ' ↳ '
+    barrier_length = len(barrier)
+    substrings = param.replace('_', ' ').split('.')
+    max_len = max([len(s) + (barrier_length * i) for i, s in enumerate(substrings)])
+    for i, s in enumerate(substrings):
+        current_line = ""
+
+        if i > 0:
+            current_line += ' ' * barrier_length * (i-1)
+            current_line += barrier
+
+        current_line += s
+        current_line = current_line.ljust(max_len)
+        current_line += '​'
+
+        label += current_line
+        if i < len(substrings) - 1:
+            label += '<br>'
+
+    return label
+
+
 def create_new_coords(coords: np.ndarray, min_units_between: int) -> np.ndarray:
     new_coords = []
 
@@ -68,9 +89,8 @@ class HTMLReporter(Reporter):
     def write_report(self, pipeline_component: PipelineComponent) -> None:
         df = pipeline_component.load_results_dataframe()
 
-        # Prepare data for subplots
-        subplots = []
-        subplot_titles = []
+        #
+        figures = []
 
         # Images
         for i, (_, row) in enumerate(pipeline_component.get_top_results_dataframe().head(1).iterrows()):
@@ -79,35 +99,17 @@ class HTMLReporter(Reporter):
             # ROC image
             roc_file_path = os.path.join(best_job_dir_path, ROC_PLOT_FILE_NAME)
             if os.path.exists(roc_file_path):
-                img = Image.open(roc_file_path)
-                img_bytes = BytesIO()
-                img.save(img_bytes, format='PNG')
-                img_str = base64.b64encode(img_bytes.getvalue()).decode('utf-8')
-
-                subplot_titles.append(f"linear-log ROC plot of {get_ordinal(i + 1)} best job<br>{best_job_dir_path}")
-                subplots.append(go.Image(z=img_str))
+                figures.append(roc_file_path)
 
             # Energy plot image
             energy_plot_file_path = os.path.join(best_job_dir_path, ENERGY_PLOT_FILE_NAME)
             if os.path.exists(energy_plot_file_path):
-                img = Image.open(energy_plot_file_path)
-                img_bytes = BytesIO()
-                img.save(img_bytes, format='PNG')
-                img_str = base64.b64encode(img_bytes.getvalue()).decode('utf-8')
-
-                subplot_titles.append(f"Ridgeline plot of energies of {get_ordinal(i + 1)} best job<br>{best_job_dir_path}")
-                subplots.append(go.Image(z=img_str))
+                figures.append(energy_plot_file_path)
 
             # Energy plot image
             charge_plot_file_path = os.path.join(best_job_dir_path, CHARGE_PLOT_FILE_NAME)
             if os.path.exists(charge_plot_file_path):
-                img = Image.open(charge_plot_file_path)
-                img_bytes = BytesIO()
-                img.save(img_bytes, format='PNG')
-                img_str = base64.b64encode(img_bytes.getvalue()).decode('utf-8')
-
-                subplot_titles.append(f"Violin plot of charges of {get_ordinal(i + 1)} best job<br>{best_job_dir_path}")
-                subplots.append(go.Image(z=img_str))
+                figures.append(charge_plot_file_path)
 
         # Create boxplots
         boxplot_columns = sorted([
@@ -122,8 +124,7 @@ class HTMLReporter(Reporter):
 
         for column in boxplot_columns:
             fig = self.get_boxplot(df, column, pipeline_component.criterion.name)
-            subplots.append(fig)
-            subplot_titles.append(column.lstrip('parameters.').replace('.', '\n'))
+            figures.append(fig)
 
         # Create heatmap for pairs of columns
         heatmap_numeric_columns = sorted([
@@ -141,17 +142,15 @@ class HTMLReporter(Reporter):
         for column_1, column_2 in itertools.combinations(heatmap_numeric_columns, 2):
             df_no_duplicates = df.drop_duplicates(subset=[column_1, column_2], keep="first", ignore_index=True)
             fig = self.get_heatmap(df_no_duplicates, column_1, column_2, pipeline_component.criterion.name)
-            subplots.append(fig)
-            subplot_titles.append(f"{column_2} vs. {column_1}")
+            figures.append(fig)
 
         # Generate the HTML report
-        html = self.get_html(subplots, subplot_titles)
-
+        html = self.get_html(figures)
         with open(os.path.join(pipeline_component.component_dir.path, self.report_file_name), 'w') as f:
             f.write(html)
 
     @staticmethod
-    def get_boxplot(df: pd.DataFrame, column: str, criterion_name: str, order=None) -> go.Figure:
+    def get_boxplot(df: pd.DataFrame, column: str, criterion_name: str, order=None, title=None) -> go.Figure:
         if order is None:
             order = sorted(df[column].unique())
 
@@ -179,11 +178,17 @@ class HTMLReporter(Reporter):
             data.append(subplot)
 
         layout = go.Layout(
-            xaxis=dict(title=column),
-            yaxis=dict(title=criterion_name),
+            font_family='monospace',
+            xaxis=dict(title=get_axis_label(column.replace('parameters.', ''))),
+            yaxis=dict(title=get_axis_label(criterion_name)),
             boxmode="group"
         )
         fig = go.Figure(data=data, layout=layout)
+
+        #
+        if title is not None:
+            fig.update_layout(title_text=title, title_xanchor="auto")
+
         return fig
 
     @staticmethod
@@ -212,94 +217,128 @@ class HTMLReporter(Reporter):
                                          line=dict(color='gray', width=1)))
 
         # Combine the heatmap and scatter plot
-        fig = go.Figure(data=[heatmap, scatter])
+        layout = go.Layout(
+            font_family='monospace',
+            xaxis=dict(title=get_axis_label(x.replace('parameters.', ''))),
+            yaxis=dict(title=get_axis_label(y.replace('parameters.', ''))),
+            boxmode="group"
+        )
+        fig = go.Figure(data=[heatmap, scatter], layout=layout)
 
         #
-        fig.update_layout(xaxis_title=x, yaxis_title=y)
         if title is not None:
-            fig.update_layout(title_text=title)
+            fig.update_layout(title_text=title, title_xanchor="auto")
 
         return fig
 
     @staticmethod
-    def get_html(subplots, subplot_titles):
+    def get_html(figures: Union[go.Figure, str]):
         divs = []
-        for i, (subplot, subplot_title) in enumerate(zip(subplots, subplot_titles)):
-            fig = go.Figure(subplot)
-            fig.update_layout(title=subplot_title, margin=dict(l=20, r=20, t=40, b=20))
-            plot_html = fig.to_html(full_html=False, include_plotlyjs=False)
-            divs.append(f"""<div>
-        <label for="heightSlider{i}">Height:</label>
-        <input type="range" min="100" max="1000" value="300" class="slider" id="heightSlider{i}">
-        <label for="widthSlider{i}">Width:</label>
-        <input type="range" min="100" max="1000" value="500" class="slider" id="widthSlider{i}">
-        <div id="plot{i}">{plot_html}</div>
-    </div>
-"""
-                        )
+
+        for i, fig in enumerate(figures):
+            is_one_slider = bool(isinstance(fig, str))
+            if is_one_slider:
+                image_path = fig
+                with open(image_path, "rb") as image_file:
+                    encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
+
+                slider_html = f"""
+                    <label for="sizeSlider{i}">Size:</label>
+                    <input type="range" min="100" max="1000" value="300" class="slider" id="sizeSlider{i}">
+                    """
+                divs.append(f"""<div>
+                    {slider_html}
+                    <img id="image{i}" src="data:image/jpeg;base64,{encoded_image}" style="max-width: 100%; max-height: 100%;">
+                </div>
+                """)
+            else:
+                plot_html = fig.to_html(full_html=False, include_plotlyjs=False)
+                slider_html = f"""
+                    <label for="heightSlider{i}">Height:</label>
+                    <input type="range" min="100" max="1000" value="300" class="slider" id="heightSlider{i}">
+                    <label for="widthSlider{i}">Width:</label>
+                    <input type="range" min="100" max="1000" value="500" class="slider" id="widthSlider{i}">
+                    """
+
+                divs.append(f"""<div>
+                {slider_html}
+                <div id="plot{i}">{plot_html}</div>
+            </div>
+        """
+                            )
 
         html = f"""
-<!DOCTYPE html>
-<html>
-<head>
-    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-    <style>
-        .slider {{
-            width: 100%;
-        }}
-    </style>
-</head>
-<body>
-    {''.join(divs)}
-    <script>
-        function getMaxHeight() {{
-            return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-        }}
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+        <style>
+            .slider {{
+                width: 100%;
+            }}
+        </style>
+    </head>
+    <body>
+        {''.join(divs)}
+        <script>
+            function setDefaultSize(imageElement, sizeSlider, widthSlider, heightSlider) {{
+                if (sizeSlider) {{
+                    var maxSize = Math.min(window.innerHeight, window.innerWidth) * 0.5;
+                    sizeSlider.value = maxSize;
+                    var aspectRatio = imageElement.naturalWidth / imageElement.naturalHeight;
+                    imageElement.style.width = maxSize + 'px';
+                    imageElement.style.height = maxSize / aspectRatio + 'px';
+                }} else {{
+                    var defaultHeight = window.innerHeight * 0.5;
+                    var defaultWidth = window.innerWidth * 0.5;
+                    heightSlider.value = defaultHeight;
+                    widthSlider.value = defaultWidth;
+                }}
+            }}
 
-        function getMaxWidth() {{
-            return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        }}
+            for (var i = 0; i < {len(figures)}; i++) {{
+                var is_one_slider = document.getElementById('sizeSlider' + i) !== null;
 
-        function setDefaultSize(plotDiv, heightSlider, widthSlider) {{
-            var defaultHeight = getMaxHeight() * 0.5;
-            var defaultWidth = getMaxWidth() * 0.5;
-            heightSlider.value = defaultHeight;
-            widthSlider.value = defaultWidth;
-            Plotly.update(plotDiv, {{}}, {{height: defaultHeight, width: defaultWidth}});
-        }}
+                if (is_one_slider) {{
+                var imageElement = document.getElementById('image' + i);
+                var sizeSlider = document.getElementById('sizeSlider' + i);
 
-        var plots = [];
-        for (var i = 0; i < {len(subplots)}; i++) {{
-            var plotDiv = document.getElementById('plot' + i).getElementsByClassName('plotly-graph-div')[0];
-            plots.push(plotDiv);
+                sizeSlider.oninput = (function(img) {{
+                    return function() {{
+                        var size = this.value;
+                        var aspectRatio = img.naturalWidth / img.naturalHeight;
+                        img.style.width = size + 'px';
+                        img.style.height = size / aspectRatio + 'px';
+                    }};
+                }})(imageElement);
 
-            var heightSlider = document.getElementById('heightSlider' + i);
-            heightSlider.max = getMaxHeight();
-            heightSlider.oninput = (function(idx) {{
-                return function() {{
-                    var plotDiv = plots[idx];
-                    var height = document.getElementById('heightSlider' + idx).value;
-                    var width = document.getElementById('widthSlider' + idx).value;
-                    Plotly.update(plotDiv, {{}}, {{height: height, width: width}});
-                }};
-            }})(i);
+                setDefaultSize(imageElement, sizeSlider, null, null);
+                }} else {{
+                    var plotDiv = document.getElementById('plot' + i).getElementsByClassName('plotly-graph-div')[0];
+                    var heightSlider = document.getElementById('heightSlider' + i);
+                    var widthSlider = document.getElementById('widthSlider' + i);
 
-            var widthSlider = document.getElementById('widthSlider' + i);
-            widthSlider.max = getMaxWidth();
-            widthSlider.oninput = (function(idx) {{
-                return function() {{
-                    var plotDiv = plots[idx];
-                    var height = document.getElementById('heightSlider' + idx).value;
-                    var width = document.getElementById('widthSlider' + idx).value;
-                    Plotly.update(plotDiv, {{}}, {{height: height, width: width}});
-                }};
-            }})(i);
-
-            setDefaultSize(plotDiv, heightSlider, widthSlider);
-        }}
-    </script>
-</body>
-</html>
-"""
-
+                    heightSlider.oninput = (function(plot) {{
+                        return function() {{
+                            var height = this.value;
+                            Plotly.update(plot, {{}}, {{height: height}});
+                        }};
+                    }})(plotDiv);
+        
+                    widthSlider.oninput = (function(plot) {{
+                        return function() {{
+                            var width = this.value;
+                            Plotly.update(plot, {{}}, {{width: width}});
+                        }};
+                    }})(plotDiv);
+        
+                    setDefaultSize(null, null, widthSlider, heightSlider);
+                }}
+            }}
+        </script>
+    </body>
+    </html>
+        """
+    
         return html
+    
