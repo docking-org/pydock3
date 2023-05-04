421a131ac5e9a88ee7283455739426eae88bc82d
Ian Scott Knight <isk@alumni.stanford.edu>
2023-05-03 18:23:31 -0700

    improves logging

--- pydock3/dockopt/dockopt.py
+++ pydock3/dockopt/dockopt.py
@@ -820,6 +820,7 @@ class DockoptStep(PipelineComponent):
                 #max_reattempts=component_run_func_arg_set.retrodock_job_max_reattempts,  # TODO
                 export_mol2=should_export_mol2,
             )
+            logger.info("Submitting retrodock array job for {sub_dir_name}")
             sub_result, procs = array_job.submit_all_tasks(
                 skip_if_complete=True,
             )
